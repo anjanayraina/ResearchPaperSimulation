52dba1a3c21462be1c3616d63b46744aeb2b84e3
anjanay <anjanayraina326@gmail.com>
2023-06-16 00:26:51 +0530

    All the roles added

--- contracts/AuthenticationLayer.sol
+++ contracts/AuthenticationLayer.sol
@@ -1,7 +1,9 @@
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
 import "@openzeppelin/contracts/access/AccessControl.sol";
 contract AuthenticationLayer is AccessControl{
-
+    bytes32 public constant USER_ROLE = keccak256("USER");
+    bytes32 public constant MINER_ROLE = keccak256("MINER");
     constructor (address root)  {
     _setupRole(DEFAULT_ADMIN_ROLE, root);
     }
@@ -10,4 +12,14 @@ contract AuthenticationLayer is AccessControl{
         require(hasRole(DEFAULT_ADMIN_ROLE , msg.sender)  , "You dont have the required role");
         _;
     }
+
+    modifier isMiner(){
+        require(hasRole(MINER_ROLE , msg.sender)  , "You dont have the required role");
+        _;
+    } 
+
+    modifier isUser(){
+        require(hasRole(USER_ROLE , msg.sender)  , "You dont have the required role");
+        _;
+    } 
 }
