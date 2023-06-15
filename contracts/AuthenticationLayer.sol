5f4e9e6320a9f2ae21995bf0b473266704c573e9
anjanay <anjanayraina326@gmail.com>
2023-06-16 00:20:07 +0530

    Modifier added

--- contracts/AuthenticationLayer.sol
+++ contracts/AuthenticationLayer.sol
@@ -1,6 +1,13 @@
 pragma solidity 0.8.9;
-import "@openzeppelin/contracts/ownership/Ownable.sol";
-contract AuthenticationLayer{
+import "@openzeppelin/contracts/access/AccessControl.sol";
+contract AuthenticationLayer is AccessControl{
 
-    
+    constructor (address root)  {
+    _setupRole(DEFAULT_ADMIN_ROLE, root);
+    }
+
+    modifier isOwner(){
+        require(hasRole(DEFAULT_ADMIN_ROLE , msg.sender)  , "You dont have the required role");
+        _;
+    }
 }
